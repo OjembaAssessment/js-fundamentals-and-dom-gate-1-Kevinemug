a2c04c3b811f3fae97b2317e318b6c6f3aa9d32c
unknown <Kevinemug>
2023-07-24 10:41:54 +0100

    start ,stop,and reset functionalities

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,55 @@
-console.log("Exercise 1 - Word Highlighter");
+const most_frequent_words = function(paragraph) {
+    // Split the paragraph into words.
+    var words = paragraph.split(" ");
+  
+    // Create an object to store the number of times each word appears.
+    var word_counts = {};
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      if (word in word_counts) {
+        word_counts[word] += 1;
+      } else {
+        word_counts[word] = 1;
+      }
+    }
+  
+    // Find the 5 words that appear the most often.
+    var most_frequent_words = [];
+    for (var word in word_counts) {
+      most_frequent_words.push({
+        count: word_counts[word],
+        word: word
+      });
+    }
+    most_frequent_words.sort(function(a, b) {
+      return b.count - a.count;
+    });
+  
+    // Return the 5 most frequent words.
+    return most_frequent_words.slice(0, 6).map(function(word) {
+      return word.word;
+    });
+  };
+  
+  const highlight_words = function(paragraph, words) {
+    var highlighted_paragraph = "";
+    for (var i = 0; i < paragraph.length; i++) {
+      var char = paragraph[i];
+      if (words.includes(char)) {
+        highlighted_paragraph += "<mark style=background-color:'red '>" + char + "</mark>";
+      } else {
+        highlighted_paragraph += char;
+      }
+    }
+    return highlighted_paragraph;
+  };
+  
+  if (document.getElementById("myParagraph")) {
+    var paragraph = document.getElementById("myParagraph").textContent;
+    var words = most_frequent_words(paragraph);
+    var highlighted_paragraph = highlight_words(paragraph, words);
+  console.log(words)
+    // Set the innerHTML of the paragraph element to the highlighted paragraph.
+    document.getElementById("myParagraph").innerHTML = highlighted_paragraph;
+  }
+  
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,49 @@
 console.log("Exercise 3 - Timer");
+const input = document.getElementById("input");
+const counter = document.getElementById("result");
+const start = document.getElementById("start");
+const reset = document.getElementById("reset");
+let intervalId;
+let isTimerRunning = false;
+let display = 0;
+
+function updateDisplay() {
+  const hours = Math.floor(display / 3600);
+  const minutes = Math.floor((display % 3600) / 60);
+  const seconds = display % 60;
+  counter.innerHTML = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+function startCountdown() {
+  if (!isTimerRunning) {
+    display = Number(input.value);
+    counter.innerHTML = display;
+    if (display > 0) {
+      isTimerRunning = true;
+      start.textContent = "STOP";
+      intervalId = setInterval(() => {
+        display--;
+        updateDisplay();
+        if (display <= 0) {
+          clearInterval(intervalId);
+          isTimerRunning = false;
+          start.textContent = "START";
+          alert("Countdown completed!");
+        }
+      }, 1000);
+    }
+  } else {
+    clearInterval(intervalId);
+    isTimerRunning = false;
+    start.textContent = "START";
+  }
+}
+function resetCountdown() {
+    clearInterval(intervalId);
+    isTimerRunning = false;
+    start.textContent = "START";
+    display = 0;
+    updateDisplay();
+  }
+start.addEventListener("click", startCountdown);
+reset.addEventListener("click", resetCountdown);
