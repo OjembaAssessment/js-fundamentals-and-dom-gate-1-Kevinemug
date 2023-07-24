4da64200d395fcd9419a678fa3430f38df97c8fd
unknown <Kevinemug>
2023-07-24 12:00:41 +0100

    new changes

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,55 +1,28 @@
-const most_frequent_words = function(paragraph) {
-    // Split the paragraph into words.
-    var words = paragraph.split(" ");
-  
-    // Create an object to store the number of times each word appears.
-    var word_counts = {};
-    for (var i = 0; i < words.length; i++) {
-      var word = words[i];
-      if (word in word_counts) {
-        word_counts[word] += 1;
-      } else {
-        word_counts[word] = 1;
-      }
-    }
-  
-    // Find the 5 words that appear the most often.
-    var most_frequent_words = [];
-    for (var word in word_counts) {
-      most_frequent_words.push({
-        count: word_counts[word],
-        word: word
-      });
-    }
-    most_frequent_words.sort(function(a, b) {
-      return b.count - a.count;
-    });
-  
-    // Return the 5 most frequent words.
-    return most_frequent_words.slice(0, 6).map(function(word) {
-      return word.word;
-    });
-  };
-  
-  const highlight_words = function(paragraph, words) {
-    var highlighted_paragraph = "";
-    for (var i = 0; i < paragraph.length; i++) {
-      var char = paragraph[i];
-      if (words.includes(char)) {
-        highlighted_paragraph += "<mark style=background-color:'red '>" + char + "</mark>";
-      } else {
-        highlighted_paragraph += char;
-      }
-    }
-    return highlighted_paragraph;
-  };
-  
-  if (document.getElementById("myParagraph")) {
-    var paragraph = document.getElementById("myParagraph").textContent;
-    var words = most_frequent_words(paragraph);
-    var highlighted_paragraph = highlight_words(paragraph, words);
-  console.log(words)
-    // Set the innerHTML of the paragraph element to the highlighted paragraph.
-    document.getElementById("myParagraph").innerHTML = highlighted_paragraph;
+document.addEventListener("DOMContentLoaded",  () => {
+  const para = document.getElementById("myParagraph").textContent;
+
+  const text = para.toLowerCase().match(/\b\w+\b/g);
+  const wordCount = {};
+  for (const word of text) {
+    wordCount[word] = wordCount[word] ? wordCount[word] + 1 : 1;
   }
-  
+
+  const sorted = Object.keys(wordCount).sort(
+    (a, b) => wordCount[b] - wordCount[a]
+  );
+
+  const most = sorted.slice(0, 5);
+
+  const highlight = para.replace(
+    new RegExp(`\\b(${most.join("|")})\\b`, "gi"),
+    (match) => `<span class="highlighted">${match}</span>`
+  );
+
+  const paraUnderline = highlight.replace(
+    /\b[A-Z]\w*\b/g,
+    (match) => `<span class="underline">${match}</span>`
+  );
+
+  const paraEL = document.getElementById("myParagraph");
+  paraEL.innerHTML = paraUnderline;
+});
--- Exercise 2 - Word Cloud/cloud.js
+++ Exercise 2 - Word Cloud/cloud.js
@@ -1,1 +1,28 @@
 console.log("Exercise 2 - Word Cloud");
+document.addEventListener("DOMContentLoaded", function () {
+    const para = document.getElementById("myParagraph").textContent;
+  
+    const words = para.toLowerCase().match(/\b\w+\b/g);
+    const wordCount = {};
+    for (const word of words) {
+      wordCount[word] = wordCount[word] ? wordCount[word] + 1 : 1;
+    }
+  
+    const sorted = Object.keys(wordCount).sort(
+      (a, b) => wordCount[b] - wordCount[a]
+    );
+  
+    const most = sorted.slice(0, 12);
+  
+    // Create the word cloud elements
+    const worCont = document.getElementById("myWordCloud");
+    worCont.innerHTML = ""; // Clear the container first
+    for (let i = 0; i < most.length; i++) {
+      const elWord = document.createElement("div");
+      elWord.className = "word";
+      elWord.textContent = most[i];
+      elWord.style.fontSize = `${64 - i * 4}px`;
+      worCont.appendChild(elWord);
+    }
+  });
+  
